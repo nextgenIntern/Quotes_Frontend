d926d74ad811d10c5f579f335fc4e54e82112c77
nextgener8 <hr.nextgr8@gmail.com>
2026-01-02 10:57:32 +0530

    changes done in quotes categories submitquote

--- Script/categories.js
+++ Script/categories.js
@@ -51,7 +51,7 @@ function renderCategories(categories) {
     return;
   }
 
-  const chunkSize = 5;
+  const chunkSize = 7;
 
   for (let i = 0; i < categories.length; i += chunkSize) {
     const chunk = categories.slice(i, i + chunkSize);
